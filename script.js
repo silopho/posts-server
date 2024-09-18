a4317cbcdb042428caf85b17022cf6ad0e2e8d7d
Misha Fatuev <mishafatuev@gmail.com>
2024-09-18 08:23:03 +0300

    Getting time on GET request

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const express = require("express")
 const moment = require("moment")
+
+const app = express()
+const PORT = 8000
+const HOST = "localhost"
 
 function GetDate() {
   console.log(moment().format("YYYY/MM/DD hh:mm:ss"))
 };
 
-GetDate()
+app.get("/date", () => {
+  GetDate()
+});
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
