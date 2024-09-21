ee6dd8a6b79b2852e523d784467282f7d71f9c85
Misha Fatuev <mishafatuev@gmail.com>
2024-09-22 00:08:55 +0300

    Add html and css

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const express = require("express")
 const moment = require("moment")
+const path = require("path")
 
 const app = express()
+
 const PORT = 8000
 const HOST = "localhost"
+
+app.use('/static/', express.static(path.join(__dirname, 'static'))) 
 
 function GetDate() {
   console.log(moment().format("YYYY/MM/DD hh:mm:ss"))
 };
 
-app.get("/date", () => {
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./templates/index.html"))
+});
+
+app.get("/date", (req, res) => {
   GetDate()
 });
+
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
 });
