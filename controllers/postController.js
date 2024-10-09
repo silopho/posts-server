7e76e6d24fe24838ddad07f52b3ffe6ddafd069c
Misha Fatuev <mishafatuev@gmail.com>
2024-10-09 18:07:45 +0300

    Added post router

---
+++ controllers/postController.js
@@ -0,0 +1,30 @@
+const postService = require('../services/postService')
+
+function getAllPosts(req, res) {
+    const context = postService.getAllPosts()
+    res.render('posts', context)
+};
+
+function getPostById(req, res) {
+    const id = req.params.id
+    const context = postService.getPostById(id).context
+    if (id <= postService.posts.length && id >= 1){
+        res.render("post", context)
+      } else if (id == "create"){
+        res.render("post_create")
+      } else {
+        res.send("This page does not exist")
+      }
+};
+
+function createPost(req, res) {
+    const data = req.body
+    postService.createPost(data)
+    res.send('send');
+}
+
+module.exports = {
+    getAllPosts,
+    getPostById,
+    createPost,
+}
