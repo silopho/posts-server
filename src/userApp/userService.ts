cdcb5c0be5fd675b56b926b02247f9d783ccc148
Misha Fatuev <mishafatuev@gmail.com>
2025-02-26 13:39:37 +0200

    added password encryption in db

--- src/userApp/userService.ts
+++ src/userApp/userService.ts
@@ -1,33 +1,45 @@
 import userRepository from "./userRepository"
+import { compare, hash } from "bcrypt"
+
 import { ICreatePost, ICreateUser, IError, ISuccess, IUser } from "../types/types"
 
 async function registrationUser(data: ICreateUser): Promise< IError | ISuccess<IUser> > {
-    const userByEmail = await userRepository.getUserByEmail(data.email);
+    const userByEmail = await userRepository.getUserByEmail(data.email)
     if (userByEmail) {
-        return { status: "error", message: "email already exists" };
+        return { status: "error", message: "email already exists" }
     }
 
-    const userByUsername = await userRepository.getUserByUsername(data.username);
+    const userByUsername = await userRepository.getUserByUsername(data.username)
     if (userByUsername) {
-        return { status: "error", message: "username already exists" };
+        return { status: "error", message: "username already exists" }
     }
 
-    const newUser = await userRepository.createUser(data);
+    const hashedPassword: string = await hash(data.password, 10)
+    data.password = hashedPassword
+
+    const newUser = await userRepository.createUser(data)
+    console.log(newUser)
     if (!newUser) {
-        return { status: "error", message: "create error" };
+        return { status: "error", message: "create error" }
     }
 
-    return { status: "success", data: newUser };
+    return { status: "success", data: newUser }
 }
 
 async function loginUser(data: ICreatePost): Promise< IError | ISuccess<IUser> > {
     const user = await userRepository.getUserByEmail(data.email)
+    console.log(user)
+
     if (!user) {
         return { status: 'error', message: 'user not found'}
     }
-    if (data.password != user.password) {
+
+
+    const compared = await compare(data.password, user.password)
+    if (!compared) {
         return { status: 'error', message: 'password incorrect'}
     }
+
     return {status: 'success', data: user};
 }
 
